ac329ade768305d87eb4bb32d03fa804578af155
Elias Nikkari <eliasnikkari@gmail.com>
2024-06-13 16:52:49 +0300

    Tasks can now be clicked off with the 'Complete' Button. Next adding 'Add task' functionality.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,16 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-
-/*type User = {
-  id: string;
-  name: string;
-  avatar: string;
-}*/
-
-interface User {
-  id: string;
-  name: string;
-  avatar: string;
-}
+import { type User } from './user.model';
 
 @Component({
   selector: 'app-user',
@@ -21,12 +10,13 @@ interface User {
 })
 export class UserComponent {
   @Input({required: true}) user!: User;
+  @Input({required: true}) selected!: boolean;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
     return 'assets/users/' + this.user.avatar;
   }
-
+  
   onSelectUser() {
     this.select.emit(this.user.id);
   }
